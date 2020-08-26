323be4340ddeb61bfda85bb9b36a33162f498516
SUJIN YANG <yangmit0524@gmail.com>
2020-08-26 16:16:54 +0900

    10-fold-validation in about algorithm

--- src/main/java/edu/handong/csee/isel/daise/Simple.java
+++ src/main/java/edu/handong/csee/isel/daise/Simple.java
@@ -3,15 +3,19 @@ package edu.handong.csee.isel.daise;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.BayesNet;
 import weka.classifiers.bayes.NaiveBayes;
-import weka.classifiers.functions.Logistic;
-import weka.classifiers.lazy.IBk;
+import weka.classifiers.functions.SimpleLogistic;
+import weka.classifiers.trees.J48;
+import weka.classifiers.trees.LMT;
 import weka.classifiers.trees.RandomForest;
 import weka.core.AttributeStats;
 import weka.core.Instances;
@@ -31,37 +35,43 @@ public class Simple {
 		
 		DataSource source = new DataSource(args[0]);
 		Instances Data = source.getDataSet();
-		Data.setClassIndex(Data.numAttributes() - 1);
+		Data.setClassIndex(0);
 		System.out.println(Data.classAttribute());
 		
-		AttributeStats attStats = Data.attributeStats(Data.numAttributes() - 1);
-		System.out.println(attStats.toString());
+		AttributeStats attStats = Data.attributeStats(0);
 		
-		DataSource testSource = new DataSource(args[3]);
-		Instances testData = testSource.getDataSet();
-		testData.setClassIndex(testData.numAttributes() - 1);
-		System.out.println(testData.classAttribute());
+//		DataSource testSource = new DataSource(args[3]);
+//		Instances testData = testSource.getDataSet();
+//		testData.setClassIndex(testData.numAttributes() - 1);
+//		System.out.println(testData.classAttribute());
 		
+		ArrayList<String> algorithms = new ArrayList<String>(Arrays.asList("random","naive","j48","bayesNet","lmt","simpleLo"));
+		
+		for(String algorithm : algorithms) {
 		Classifier classifyModel = null;
 		
-		if(args[2].toString().compareTo("r") == 0) {
+		if(algorithm.compareTo("random") == 0) {
 			classifyModel = new RandomForest();
-		}else if(args[2].toString().compareTo("n") == 0){
+		}else if(algorithm.compareTo("naive") == 0){
 			classifyModel = new NaiveBayes();
-		}else if(args[2].toString().compareTo("l") == 0){
-			classifyModel = new Logistic();
-		}else if(args[2].toString().compareTo("i")==0){
-			classifyModel = new IBk();
+		}else if(algorithm.compareTo("j48") == 0){
+			classifyModel = new J48();
+		}else if(algorithm.compareTo("bayesNet") == 0){
+			classifyModel = new BayesNet();
+		}else if(algorithm.compareTo("lmt") == 0){
+			classifyModel = new LMT();
+		}else if(algorithm.compareTo("simpleLo") == 0){
+			classifyModel = new SimpleLogistic();
 		}
 		
 		classifyModel.buildClassifier(Data);
 		
 		Evaluation evaluation = new Evaluation(Data);
-//		evaluation.crossValidateModel(classifyModel, Data, 10, new Random(1));
-		evaluation.evaluateModel(classifyModel, testData);
+		evaluation.crossValidateModel(classifyModel, Data, 10, new Random(1));
 		
+//		evaluation.evaluateModel(classifyModel, testData);
 		
-		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(args[1] +File.separator + projectname + "-" + args[2] + "-10-fold.txt")));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(args[1] +File.separator + projectname + "-" + algorithm + "-10-fold.txt")));
 		String strSummary = evaluation.toSummaryString();
 		String detail = evaluation.toClassDetailsString();
 		bufferedWriter.write(Data.attribute(0).toString());
@@ -70,6 +80,9 @@ public class Simple {
 		bufferedWriter.write(strSummary);
 		bufferedWriter.write(detail);
 		bufferedWriter.close();
+		}
+		
+		System.out.println("Finish "+projectname);
 	}
 
 }
